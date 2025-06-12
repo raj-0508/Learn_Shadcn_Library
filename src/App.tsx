9a0d9b00775716baf2dd176b247e4862d2d4b4d9
Raj Singhaniya <rajsinghaniya0508@gmail.com>
2025-06-12 18:46:34 +0530

    Added MenubarDemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Button } from "@/components/ui/button"
-
+import { Button } from "@/components/ui/button";
+import { MenubarDemo } from "./components/ui/MenubarDemo";
 
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
+    <div className="flex min-h-svh flex-col items-center justify-center gap-10">
+      <MenubarDemo />
       <Button>Click me</Button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
